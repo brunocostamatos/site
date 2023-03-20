47cc78939153c69429051e823d708a448cbf2041
bruno.matos <bruno.matos@camtecnologia.com.br>
2023-03-20 19:16:31 -0300

    testando commit para branch dev

--- src/App.jsx
+++ src/App.jsx
@@ -106,7 +106,7 @@ function App() {
         <Container className={darkMode === 0 ? "texto" : "textoDarkMode"}>
           <Container id="Perfil">
             <Row>
-              Perfil
+              Perfil Branch Dev
             </Row>
             <Container>
               <Row>
